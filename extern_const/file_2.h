226cd6efc975e2364459f9e90a67bccbbdad7ea8
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-12 22:05:08 +0600

    day 8

---
+++ extern_const/file_2.h
@@ -0,0 +1,1 @@
+extern const int adashima = 69;
---
+++ extern_const/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "file_2.h"
+
+int main()
+{
+  extern const int adashima;
+  std::cout << "Adashima " << adashima << " ;)" << std::endl;
+  
+  return 0;
+}
