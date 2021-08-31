64df5db2d9ea772a99579e2d22ddef67f474f5c1
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-31 13:30:29 +0600

    day 13

--- adding_two_sales_data_objects/main.cpp
+++ adding_two_sales_data_objects/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
-#include "Sales_data.h"
+
+struct Sales_data
+{
+  std::string bookNo;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
+};
 
 int main()
 {
