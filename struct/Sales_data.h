40566456187ce63ad3afadf2083c2ada543b74f2
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-05 23:27:10 +0600

    day 15

--- struct/Sales_data.h
+++ struct/Sales_data.h
@@ -4,3 +4,16 @@ struct Sales_data
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
+echo -e "are you sure you want to delete the folder "$1": \c"
+read confirmation
+case $confirmation in
+  y )
+   rm -rf "$1"
+   ;;
+  Y )
+   rm -rf "$1"
+   ;;
+  * )
+   echo "Deletion cancelled"
+   ;;
+esac
