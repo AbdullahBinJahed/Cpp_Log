778142a681ff22c3ddd270725aec84cf9f16d7f9
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-18 13:43:43 +0600

    day 17

---
+++ string_operations/reading_and_writing_strings/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+  string s;           // empty string
+  cin >> s;           // read a whitespace-separated string into s
+  cout << s << endl;  // write s to the output
+
+  return 0;
+}
