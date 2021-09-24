24bba3d400840cc9ddfa034a2dd58292259cb96b
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-24 16:13:03 +0600

    day 22

---
+++ vector/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+#include "Sales_item.h"
+using std::vector;
+using std::string;
+
+int main()
+{
+  vector<int> ivec;              // ivec holds objects of type int
+  vector<Sales_item> Sales_vec;  // holds Sales_items
+  vector<vector<string>> file;   // vector whose elements are vectors
+
+  return 0;
+}
