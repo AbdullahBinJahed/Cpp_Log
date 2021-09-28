f82df3258a47fc2ddbd35acfb35e18073393187d
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-28 07:56:09 +0600

    day 24

--- vector/defining_and_initializing_vectors.cpp
+++ vector/defining_and_initializing_vectors.cpp
@@ -11,5 +11,26 @@ int main()
   vector<int> ivec3 = ivec;       // copy elements of ivec into ivec3
   // vector<string> svec(ivec2);  // error: svec holds strings, not ints
 
+  // List Initializing a vector //
+  vector<string> articles = {"a", "an", "the"};
+
+  // Creating a Specified number of elements //
+  vector<int> ivecs(10, -1);           // ten int elements, each initialized to -1
+  vector<string> svecs(10, "Adachi");  // ten strings; each element is "Adachi"
+
+  // Value Initialization //
+  vector<int> iveci(10);  // ten elements, each initialized to 0
+  vector<string> sveci(10);  // ten elements, each an empty string
+
+  // List Initializer or Element Count? //
+  vector<int> v1(10);               // v1 has ten elements with value 0
+  vector<int> v2{10};               // v2 has one element with value 10
+  vector<int> v3(10, 1);            // v3 has ten elements with value 1
+  vector<int> v4{10, 1};            // v4 has two elements with values 10 and 1
+  vector<string> v5{"Adachi"};      // list initialization: v5 has one element
+  // vector<string> v6("Adachi");   // error: can't construct a vector from a string literal
+  vector<string> v7{10};            // v7 has ten default-initialized elements
+  vector<string> v8{10, "Adachi"};  // v8 has ten elements with value "Adachi"
+
   return 0;
 }
