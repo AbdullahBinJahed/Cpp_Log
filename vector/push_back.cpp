e77aaca0621f8f466cf0bee438cdeab454b6f2f9
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-09-30 07:33:47 +0600

    day 26

--- vector/push_back.cpp
+++ vector/push_back.cpp
@@ -1,12 +1,23 @@
+#include <iostream>
 #include <vector>
+#include <string>
 using std::vector;
+using std::string;
+using std::cin;
 
 int main()
 {
-  vector<int> v1;  // empty vector
+  vector<int> v1;          // empty vector
   for (int i = 0; i != 100; ++i)
-    v1.push_back(i);  // append sequential integers to v1
+    v1.push_back(i);       // append sequential integers to v1
   // at the end of the loop v1 has 100 elements, values 0 ... 99
+
+
+  // read words from the standard input and store them as elements in a vector
+  string word;
+  vector<string> text;     // empty vector
+  while (cin >> word)
+    text.push_back(word);  // append word to text
 
   return 0;
 }
