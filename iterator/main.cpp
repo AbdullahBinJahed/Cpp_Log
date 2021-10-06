bae9dbbe466bb84505564e9bee9b96816094e955
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-10-06 16:09:17 +0600

    day 30

--- iterator/main.cpp
+++ iterator/main.cpp
@@ -1,15 +1,20 @@
+#include <iostream>
 #include <cctype>
 #include <string>
+using std::cout;
+using std::endl;
 using std::string;
 
 int main()
 {
   string s("adachi suki");
+  cout << s << endl;
   if (s.begin() != s.end())   // make sure s is not empty
   {
     auto it = s.begin();      // it denotes the first character in s
     *it = std::toupper(*it);  // make that character uppercase
   }
+  cout << s << endl;
 
   return 0;
 }
