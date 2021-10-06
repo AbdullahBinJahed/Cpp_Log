4f1b8c7decd18b7cd7085b87881018f5ee3558de
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-10-06 15:30:31 +0600

    day 30

---
+++ iterator/main.cpp
@@ -0,0 +1,14 @@
+#include <cctype>
+#include <string>
+using std::string;
+
+int main()
+{
+  string s("adachi suki");
+  if (s.begin() != s.end())
+  {
+    auto it = s.begin();
+    *it = std::toupper(*it);
+  }
+  return 0;
+}
